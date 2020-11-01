76496dc6bb858832576429413ff205a3527bc8be
AnelZhaksybergenova <anelbolatbek1@gmail.com>
2020-11-01 10:23:50 -0600

    added more linme

--- src/test/java/Superman1.java
+++ src/test/java/Superman1.java
@@ -1,5 +1,8 @@
 public class Superman1 {
     public static void main(String[] args) {
         System.out.println("first one");
+        //batman has cool bat mobile
+        //lol
+        //need to put something
     }
 }
