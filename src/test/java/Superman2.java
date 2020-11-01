443b61742ba91b1e7ed402caa4810be29dae2d25
AnelZhaksybergenova <anelbolatbek1@gmail.com>
2020-11-01 10:49:42 -0600

    more lines of words

--- src/test/java/Superman2.java
+++ src/test/java/Superman2.java
@@ -2,6 +2,8 @@ public class Superman2 {
     public static void main(String[] args) {
         System.out.println("Second commit");
         //hi
+        //added some changes
+        //practice
 
     }
 }
